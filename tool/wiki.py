08734da01c3ead1bc1c7c8b19ed9a5ac7d64af43
s-nt-s <santos82h@gmail.com>
2026-05-10 22:21:31 +0200

    wikipedia

--- tool/wiki.py
+++ tool/wiki.py
@@ -13,30 +13,37 @@ config_log(
 )
 
 logger = logging.getLogger(__name__)
-re_imdb = re.compile(r"^tt\d{3,}$")
 
 
 ST = SparqlTsv(
     endpoint="https://query.wikidata.org/sparql",
     user_agent=f'ImdbBoot/0.0 ({G.remote}; {G.mail})',
-    max_retries=100
+    max_retries=3
 )
+
+def safe_query(query: str, match: re.Pattern):
+    try:
+        yield from ST.query(query, match=match)
+    except:
+        pass
 
 
 def get_dict(
     query: str,
-    re_v: re.Pattern,
+    rgx: re.Pattern,
     cs_v: Optional[Callable[[str], Any]] = None
 ):
     k_v: dict[str, set] = defaultdict(set)
     v_k: dict[str, set] = defaultdict(set)
-    for k, v in ST.query(query):
-        if k and v and re_imdb.match(k) and re_v.match(v):
-            if cs_v:
-                v = cs_v(v)
-            if v is not None:
-                k_v[k].add(v)
-                v_k[v].add(k)
+    for k, v in safe_query(
+        query,
+        match=rgx
+    ):
+        if cs_v:
+            v = cs_v(v)
+        if v is not None:
+            k_v[k].add(v)
+            v_k[v].add(k)
     obj: dict[str, str] = {}
     for k, vals in k_v.items():
         if len(vals)!=1:
@@ -50,14 +57,14 @@ def get_dict(
 
 def refresh(
     name: str,
-    re_v: re.Pattern,
+    rgx: re.Pattern,
     cs_v: Optional[Callable[[str], Any]] = None
 ):
     obj = FM.load(f"rec/{name}.dct.txt")
     logger.info(f"{name} = {len(obj)} items")
     for k, v in get_dict(
         FM.load(f"sparql/{name}.lite.sparql"),
-        re_v=re_v,
+        rgx=rgx,
         cs_v=cs_v
     ).items():
         obj[k] = v
@@ -68,12 +75,12 @@ def refresh(
 if __name__ == "__main__":
     refresh(
         "filmaffinity",
-        re_v=re.compile(r"^\d{3,}$"),
+        rgx=re.compile(r"^tt\d{3,}\t\d{3,}$"),
         cs_v=int
     )
     refresh(
         "wikipedia",
-        re_v=re.compile(r"^https://es.wikipedia.org/wiki/.+$"),
+        rgx=re.compile(r"^tt\d{3,}\thttps://es.wikipedia.org/wiki/.+$"),
     )
     
     
